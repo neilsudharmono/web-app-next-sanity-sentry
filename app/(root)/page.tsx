d8b31d46e6e54cfb2f4101ecfe36c24bf30eed76
Neil Casaandra Sudharmono <39051247+neilsudharmono@users.noreply.github.com>
2025-06-17 09:48:19 +1000

    JWT and Sessions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,7 @@ import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { auth } from "@/auth";
 
 export default async function Home({
   searchParams,
@@ -12,9 +13,13 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
   const params = { search: query || null };
+
+  const session = await auth();
+  console.log("Session : ", session?.id);
+
   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
-  console.log(JSON.stringify(posts, null, 2));
+  //console.log(JSON.stringify(posts, null, 2));
 
   return (
     <>
